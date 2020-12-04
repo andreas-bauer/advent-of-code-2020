9a0981f1184b3a2ac23c83b971f1c17fcf4377b7
Andreas Bauer <dev@andreasbauer.org>
2020-12-05 00:05:21 +0100

    Add solution for day 3

--- day_3/main.py
+++ day_3/main.py
@@ -4,24 +4,21 @@ with open("input.txt") as f:
 
 width = 31
 
-next_pos = 3
-hit_trees = 0
-line_counter = 2
+def calc_hits(right_steps, down_steps, lines):
+    right_pos = right_steps
+    down_pos = down_steps
+    trees_hit = 0
+    while down_pos < len(lines):
+        if lines[down_pos][right_pos] == '#':
+            trees_hit += 1
 
-def hit_symbol(char):
-    if char == "#":
-        return "X"
-    return "O"
+        down_pos += down_steps
+        right_pos = (right_pos + right_steps) % width
 
-for line in lines[1:]:
-    if line[next_pos] == '#':
-        hit_trees += 1
+    return trees_hit
 
-    next_pos = (next_pos + 3) % width
-    line_counter += 1
-    
-    symbol = hit_symbol(line[next_pos])
-    hit_line = line[:next_pos] + symbol + line[next_pos + 1:]
-    print(hit_line)
-
-print("Hit trees on the way down: " + str(hit_trees))
+print("Hit trees on the way down (1 right, 1 down): " + str(calc_hits(1, 1, lines)))
+print("Hit trees on the way down (3 right, 1 down): " + str(calc_hits(3, 1, lines)))
+print("Hit trees on the way down (5 right, 1 down): " + str(calc_hits(5, 1, lines)))
+print("Hit trees on the way down (7 right, 1 down): " + str(calc_hits(7, 1, lines)))
+print("Hit trees on the way down (1 right, 2 down): " + str(calc_hits(1, 2, lines)))
