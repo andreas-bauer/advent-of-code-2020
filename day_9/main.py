e7c41b1b044e55500b1ab642811a06819af643a4
Andreas Bauer <dev@andreasbauer.org>
2021-07-25 02:04:21 +0200

    Add solution for day 9 part 2

--- day_9/main.py
+++ day_9/main.py
@@ -1,6 +1,7 @@
 input = open('input.txt', 'r').read().split("\n")
 
 preamble_length = 25
+invalid = 0
 
 for i in range(preamble_length, len(input)):
 	current = int(input[i])
@@ -10,7 +11,29 @@ for i in range(preamble_length, len(input)):
 			sum = int(input[j]) + int(input[k])
 			if sum == current:
 				found = True
-				print(input[i] + " = " + input[j] + " + " + input[k])
 	if not found:
+		invalid = int(input[i])
 		print("No match found for " + input[i])
 		break
+
+
+def find_sequence(search):
+	for i in range(0, len(input)) :
+		sum_window = []
+		sum = 0
+		for k in range(i, len(input)):
+			value = int(input[k])
+			sum += value
+			sum_window.append(value)
+			if sum == search:
+				return sum_window
+			if sum > search:
+				break
+
+sequence = find_sequence(invalid)
+min_val = min(sequence)
+max_val = max(sequence)
+print(sequence)
+print("min = " + str(min_val))
+print("max = " + str(max_val))
+print("min + max = " + str(min_val + max_val))
